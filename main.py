faa4cd76aa1f00bfdff23d4eaf286bd662b804df
Zoacys <312312@qq.com>
2022-08-18 02:54:36 +0800

    little

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+def ok():
+    pass
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    a=1
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
